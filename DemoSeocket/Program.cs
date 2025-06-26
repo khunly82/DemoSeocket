c9538a7d10c1f68ae14e63bfe281f10da11a26b0
khun ly <khun.ly@bstorm.be>
2025-06-26 13:56:45 +0200

    add cors

--- DemoSeocket/Program.cs
+++ DemoSeocket/Program.cs
@@ -3,6 +3,9 @@ using DemoSeocket.Hubs;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSignalR();
+builder.Services.AddCors(b => b.AddDefaultPolicy(o => 
+    o.WithOrigins("http://localhost:5173").AllowAnyMethod().AllowCredentials().AllowAnyHeader()
+));
 
 // Add services to the container.
 
